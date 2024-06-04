0ddde49e2207705eca32c2cc01a7329a8cb7286f
kyle <chitmeo@chitmeo.com>
2024-06-04 17:16:24 +0700

    feat: Add DeleteCategoryAsync method to ICategoryService
    This commit adds the `DeleteCategoryAsync` method to the `ICategoryService` interface in the `Dev.Plugin.Blog.Core.Services` namespace. This method allows for the deletion of a category by its ID.

--- src/plugins/Dev.Plugin.Blog.Core/Services/ICategoryService.cs
+++ src/plugins/Dev.Plugin.Blog.Core/Services/ICategoryService.cs
@@ -3,6 +3,7 @@
 public interface ICategoryService
 {
     Task<Guid> CreateCategoryAsync(Category request);
+    Task DeleteCategoryAsync(Guid id);
     Task<Category?> GetCategoryByIdAsync(Guid id);
     Task<bool> IsCategoryNameUniqueAsync(string name);
     Task UpdateCategoryAsync(Category category);
